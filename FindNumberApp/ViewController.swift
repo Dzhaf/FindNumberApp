09e385757550573d6de8105887e24b5412b30ac2
Dzhafar <iamdzhafar@gmail.com>
2022-02-03 23:44:27 +0300

    Initial Commit

---
+++ FindNumberApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FindNumberApp
+//
+//  Created by Jafar on 03.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

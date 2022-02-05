887ae6f1294a054fc80335f03ffc20ea9afde524
Dzhafar <iamdzhafar@gmail.com>
2022-02-05 22:27:24 +0300

    Add timer

--- FindNumberApp/Game.swift
+++ FindNumberApp/Game.swift
@@ -7,8 +7,99 @@
 
 import Foundation
 
+enum StatusGame {
+    case start
+    case win
+    case lose
+}
+
+
+
+
 class Game {
     
-    private let data = [1...99]
+    struct Item {
+        var title: String
+        var isFound: Bool = false
+    }
     
+    private let data = Array(1...99)
+    var items:[Item] = []
+    private var countItems: Int
+    
+    var nextItem: Item?
+    var status: StatusGame = .start {
+        didSet {
+            if status != .start {
+                stopGame()
+            }
+        }
+    }
+    
+    private var timeForGame: Int {
+        didSet {
+            if timeForGame == 0 {
+                status = .lose
+            }
+            updateTimer(status, timeForGame)
+        }
+    }
+    private var timer: Timer?
+    private var updateTimer: ((StatusGame, Int) -> Void)
+    
+    init(countItems: Int, time: Int, updateTimer: @escaping (_ status: StatusGame, _ seconds: Int) -> Void) {
+        self.countItems = countItems
+        self.timeForGame = time
+        self.updateTimer = updateTimer
+        setupGame()
+    }
+    
+    private func setupGame() {
+        var digits = data.shuffled()
+        
+        while items.count < countItems {
+            let item = Item(title: String(digits.removeFirst()))
+            items.append(item)
+        }
+        
+        nextItem = items.shuffled().first
+        updateTimer(status, timeForGame)
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { [weak self] (_) in
+            self?.timeForGame -= 1
+            
+        })
+        
+        
+        
+        
+    }
+    func check(index: Int) {
+        
+        if items[index].title == nextItem?.title {
+            items[index].isFound = true
+            nextItem = items.shuffled().first(where: { (item) -> Bool in
+                item.isFound == false
+            })
+        }
+        
+        if nextItem == nil {
+            status = .win
+        }
+    }
+    
+    private func stopGame() {
+        timer?.invalidate()
+        
+    }
+    
+}
+
+extension Int{
+    func secondsToString() -> String {
+        let minutes = self / 60
+        let seconds = self % 60
+        
+        return String(format: "%d:%02d", minutes, seconds)
+    }
 }
--- FindNumberApp/GameViewController.swift
+++ FindNumberApp/GameViewController.swift
@@ -10,20 +10,65 @@ import UIKit
 class GameViewController: UIViewController {
     
     @IBOutlet var buttons: [UIButton]!
-   
+    @IBOutlet weak var nextDigit: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    
+    lazy var game = Game(countItems: buttons.count, time: 30)  { [weak self] (status, time) in
+        guard let self = self else {return}
+        self.timerLabel.text = time.secondsToString()
+        self.updateInfoGame(with: status)
+    }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupScreen()
        
         
     }
     
     
     @IBAction func pressButton(_ sender: UIButton) {
-        sender.isHidden = true
+        guard let buttonIndex = buttons.firstIndex(of: sender) else {return}
+        game.check(index: buttonIndex)
+        updateUI()
+    }
+    
+    private func setupScreen() {
+        for index in game.items.indices {
+            buttons[index].setTitle(game.items[index].title, for: .normal)
+            buttons[index].isHidden = false
+        }
+        
+        nextDigit.text = game.nextItem?.title
     }
     
     
+    private func updateUI() {
+        for index in game.items.indices {
+            buttons[index].isHidden = game.items[index].isFound
+            
+            
+        }
+        nextDigit.text = game.nextItem?.title
+        updateInfoGame(with: game.status)
+    }
+    
+    private func updateInfoGame(with status: StatusGame) {
+        switch status {
+        case .start:
+            statusLabel.text = "Игра началась"
+            statusLabel.textColor = .black
+        case .win:
+            statusLabel.text = "Вы выиграли"
+            statusLabel.textColor = .green
+        case .lose  :
+            statusLabel.text = "Вы проиграли"
+            statusLabel.textColor = .red
+        }
+    }
     
 }
